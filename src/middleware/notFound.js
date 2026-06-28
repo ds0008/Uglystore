2d4f87527087b663fd4727f59bbcca3156889c49
ds0008 <aasiq.ahosun.dip17@gmail.com>
2026-06-28 22:09:06 +0600

    Delete src directory

--- src/middleware/notFound.js
+++
@@ -1,7 +0,0 @@
-const AppError = require("../utils/AppError");
-
-const notFound = (req, res, next) => {
-  next(AppError.notFound(`Route not found: ${req.method} ${req.originalUrl}`));
-};
-
-module.exports = notFound;
